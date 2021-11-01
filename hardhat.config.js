2a0cc990a34a2b5e75e97ddb06417d513baf53c6
johnantony <johna@qburst.com>
2021-11-01 12:46:14 +0530

    --config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,13 +7,13 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/piDldoOhc8ML56f4BRIwxglGic9k7gn1',
-      accounts: ['f6a699afb7152c7e99d1f53412b9a5fb94a6ce6a59841240d7aa162670ee4ad8'],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
     mainnet: {
       chainId: 1,
-      url: 'https://eth-mainnet.alchemyapi.io/v2/piDldoOhc8ML56f4BRIwxglGic9k7gn1',
-      accounts: ['f6a699afb7152c7e99d1f53412b9a5fb94a6ce6a59841240d7aa162670ee4ad8'],
+      url:  process.env.PROD_ALCHEMY_KEY,
+      accounts:[process.env.PRIVATE_KEY] ,
     },
   },
 };
